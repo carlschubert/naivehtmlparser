1cc7cfec514d0dc73726ba1b4c5676b1de292173
carlschubert <carl.schubert@gmail.com>
2018-12-07 14:55:13 -0500

    Update main parser function to match new grammer

--- src/html_parser.rs
+++ src/html_parser.rs
@@ -29,39 +29,14 @@ pub fn parse_document(file: &str) -> Result<Document, Error> {
         match part.as_rule() {
             Rule::doctype => {
                 strict = true;
-                doctype = Node::Doctype(
-                    part.into_inner()
+                doctype = Node::Doctype(part.into_inner()
                         .next()
                         .unwrap() // doctype_type
                         .as_str()
-                        .to_string()
-                );
+                        .to_string());
             }
-            Rule::element => {
-                let mut inner = part
-                    .into_inner()
-                    .next()
-                    .unwrap() // tag
-                    .into_inner()
-                    .next()
-                    .unwrap() // insides
-                    .into_inner();
-                root = Node::Element((
-                    Element::new(
-                        inner
-                            .next()
-                            .unwrap() // opentag
-                            .into_inner()
-                            .next()
-                            .unwrap() // tagname
-                            .as_str(),
-                        None,
-                    ),
-                    inner
-                        .filter(|p| p.as_rule() == Rule::element)
-                        .map(parse_pair)
-                        .collect::<Result<Vec<Node>, Error>>()?,
-                ))
+            Rule::node => {
+                root = parse_pair(part)?;
             }
             _ => return Err(invalid_html()),
         }
@@ -74,33 +49,51 @@ pub fn parse_document(file: &str) -> Result<Document, Error> {
 }
 
 fn parse_pair(pair: Pair<Rule>) -> Result<Node, Error> {
-    println!("foo {:#?}", &pair.as_rule());
-
-    let foo = match pair.as_rule() {
-        Rule::element => {
-            // println!("{:?} {}", pair.as_rule(), pair.as_str());
-
-            Node::Element((
-                Element::new("foo", None),
-                pair.into_inner()
-                    .map(parse_pair)
-                    .collect::<Result<Vec<Node>, Error>>()?,
-            ))
+    match pair.as_rule() {
+        Rule::node => {
+            let mut children = Nodes::new();
+            let mut tagname = "";
+            for inner in pair.into_inner() {
+                match inner.as_rule() {
+                    Rule::text => children.push(Node::Text(inner.as_str().to_string())),
+                    Rule::comment => children.push(Node::Comment(inner.as_str().to_string())),
+                    Rule::tag => {
+                        for tag_inner in inner.into_inner() {
+                            match tag_inner.as_rule() {
+                                Rule::opentag => {
+                                    tagname = tag_inner.into_inner().next().unwrap().as_str();
+                                    println!("Opening {}", tagname);
+                                }
+                                Rule::closetag => {
+                                    println!(
+                                        "Closing {}",
+                                        tag_inner.into_inner().next().unwrap().as_str()
+                                    );
+                                }
+                                Rule::nodes => {
+                                    children.extend(
+                                        tag_inner.into_inner().map(parse_pair).collect::<Result<
+                                            Nodes,
+                                            Error,
+                                        >>(
+                                        )?,
+                                    );
+                                }
+                                Rule::single => {}
+                                _ => return Err(invalid_html()),
+                            }
+                        }
+                    }
+                    _ => return Err(invalid_html()),
+                }
+            }
+            Ok(Node::Element((Element::new(tagname, None), children)))
         }
-        Rule::text => {
-            // println!("{:?} {}", pair.as_rule(), pair.as_str());
-
-            Node::Text(pair.as_str().to_string())
-        }
-        // Rule::document => parse_pair(pair.into_inner())?,
-        Rule::tag => parse_pairs(pair.into_inner())?,
-        Rule::insides => parse_pairs(pair.into_inner())?,
-        _ => {
-            // println!("{:?} {}", pair.as_rule(), pair.as_str());
-            Node::Text("Shut up".to_string())
-        }
-    };
-    Ok(foo)
+        Rule::text => Ok(Node::Text(pair.as_str().to_string())),
+        Rule::tag => parse_pairs(pair.into_inner()),
+        Rule::nodes => parse_pairs(pair.into_inner()),
+        _ => return Err(invalid_html()),
+    }
 }
 
 fn parse_pairs(mut pairs: Pairs<Rule>) -> Result<Node, Error> {
